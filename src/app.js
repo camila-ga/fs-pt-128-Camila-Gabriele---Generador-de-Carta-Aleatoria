c05f4f90af7a60c2296e357a0c7b1691c9ea64fb
Camila Gabriele <camilagabriele@outlook.com.ar>
2025-12-04 13:07:38 +0000

    cambios

--- src/app.js
+++ src/app.js
@@ -1,25 +1,40 @@
-let valores = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "K", "Q", "A"]
-let palos = ["♦", "♥", "♠", "♣"]
-
-function eleccionAutomatica(arr) {
-  const aleatorio = Math.floor(Math.random() * arr.length)
-  return arr[aleatorio]
-}
-
-const numeroAleatorio = eleccionAutomatica(valores)
-const simboloAleatorio = eleccionAutomatica(palos)
-const color = (simboloAleatorio === "♥" || simboloAleatorio === "♦") ? 'text-danger' : 'text-dark'
-
-const cardContent = document.getElementById("card-content")
-cardContent.classList.add(color)
+window.myClickFunction = function myClickFunction() {
 
 
-const simboloStart = document.getElementById("palo-arriba")
-simboloStart.innerHTML = simboloAleatorio
+  let valores = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "K", "Q", "A"]
+  let palos = ["♦", "♥", "♠", "♣"]
 
-const numeroCenter = document.getElementById("numero-carta")
-numeroCenter.innerHTML = numeroAleatorio
-numeroCenter.classList.add(color)
 
-const simboloEnd = document.getElementById("palo-abajo")
-simboloEnd.innerHTML = simboloAleatorio
+  function eleccionAutomatica(arr) {
+    const aleatorio = Math.floor(Math.random() * arr.length)
+    return arr[aleatorio]
+  }
+
+  const numeroAleatorio = eleccionAutomatica(valores)
+  const simboloAleatorio = eleccionAutomatica(palos)
+  // const color = (simboloAleatorio === "♥" || simboloAleatorio === "♦") ? 'text-danger' : 'text-dark'
+
+  // const cardContent = document.getElementById("card-content")
+  // cardContent.classList.add(color)
+
+
+  const color = document.querySelector("#card-content")
+  const numeroColor = document.querySelector("#numero-carta")
+
+  if (simboloAleatorio === "♥" || simboloAleatorio === "♦") 
+    { color.classList.add("rojo")
+      numeroColor.classList.add("rojo") }
+  else { color.classList.remove("rojo")
+    numeroColor.classList.remove("rojo")}
+
+  const simboloStart = document.getElementById("palo-arriba")
+  simboloStart.innerHTML = simboloAleatorio
+
+  const numeroCenter = document.getElementById("numero-carta")
+  numeroCenter.innerHTML = numeroAleatorio
+  // numeroCenter.classList.add(color)
+
+  const simboloEnd = document.getElementById("palo-abajo")
+  simboloEnd.innerHTML = simboloAleatorio
+
+};
